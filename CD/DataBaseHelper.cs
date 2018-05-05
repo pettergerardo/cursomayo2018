9bf355a65272971f180f2f632697bfbecec62369
pettergerardo <petter.gerardo@gmail.com>
2018-05-05 13:28:26 -0600

    se complementa clase producto con contructores y funciones.

---
+++ CD/DataBaseHelper.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CD
+{
+    public static class DataBaseHelper
+    {
+#if DEBUG
+        static string _connectionString = "Server=148.234.16.54;Database=dbPuntoVenta;User Id=ceti;Password=ceti;";
+#else
+        static string _connectionString = "Server=148.234.16.54;Database=dbRadioShack;User Id=ceti;Password=ceti;";
+#endif
+
+        #region "FILL DATA TABLE"
+
+        public static void Fill(DataTable dataTable, String procedureName)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(procedureName, oConnection);
+            oCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter oAdapter = new SqlDataAdapter();
+
+            oAdapter.SelectCommand = oCommand;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    oAdapter.SelectCommand.Transaction = oTransaction;
+                    oAdapter.Fill(dataTable);
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oAdapter.Dispose();
+                }
+            }
+        }
+
+        public static void Fill(DataTable dataTable, String procedureName, Dictionary<string, object> dictionary)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+
+            foreach (var par in dictionary)
+            {
+                parameters.Add(new SqlParameter(par.Key, par.Value));
+            }
+
+            Fill(dataTable, procedureName, parameters.ToArray());
+        }
+
+        public static void Fill(DataTable dataTable, String procedureName, SqlParameter[] parameters)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(procedureName, oConnection);
+            oCommand.CommandType = CommandType.StoredProcedure;
+
+            if (parameters != null)
+                oCommand.Parameters.AddRange(parameters);
+
+            SqlDataAdapter oAdapter = new SqlDataAdapter();
+
+            oAdapter.SelectCommand = oCommand;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    oAdapter.SelectCommand.Transaction = oTransaction;
+                    oAdapter.Fill(dataTable);
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oAdapter.Dispose();
+                }
+            }
+        }
+
+        #endregion//DATA TABLE REPRESENTA INFORMACIÓN QUE OBTENEMOS DE LA BASE DE DATOS {ROWS & COLUMNAS}, ES UNA TABLA SOLAMENTE
+
+        #region "FILL DATASET"
+
+        public static void Fill(DataSet dataSet, String procedureName)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(procedureName, oConnection);
+            oCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter oAdapter = new SqlDataAdapter();
+
+            oAdapter.SelectCommand = oCommand;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    oAdapter.SelectCommand.Transaction = oTransaction;
+                    oAdapter.Fill(dataSet);
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oAdapter.Dispose();
+                }
+            }
+        }
+
+        public static void Fill(DataSet dataSet, String procedureName, SqlParameter[] parameters)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(procedureName, oConnection);
+            oCommand.CommandType = CommandType.StoredProcedure;
+
+            if (parameters != null)
+                oCommand.Parameters.AddRange(parameters);
+
+            SqlDataAdapter oAdapter = new SqlDataAdapter();
+
+            oAdapter.SelectCommand = oCommand;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    oAdapter.SelectCommand.Transaction = oTransaction;
+                    oAdapter.Fill(dataSet);
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oAdapter.Dispose();
+                }
+            }
+        }
+
+        #endregion//EL DATA SET CONTIENE DATA TABLES Y SE USA CUANDO SE NECESITAN VARIAS TABLAS EN UNA CONSULTA
+
+        #region "EXECUTE SCALAR"
+
+        public static object ExecuteScalar(String procedureName)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(procedureName, oConnection);
+
+            oCommand.CommandType = CommandType.StoredProcedure;
+            object oReturnValue;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    oCommand.Transaction = oTransaction;
+                    oReturnValue = oCommand.ExecuteScalar();
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oCommand.Dispose();
+                }
+            }
+            return oReturnValue;
+        }
+
+        public static object ExecuteScalar(string procedureName, Dictionary<string, object> dictionary)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+
+            foreach (var par in dictionary)
+            {
+                parameters.Add(new SqlParameter(par.Key, par.Value));
+            }
+
+            return ExecuteScalar(procedureName, parameters.ToArray());
+        }
+
+        public static object ExecuteScalar(String procedureName, SqlParameter[] parameters)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(procedureName, oConnection);
+
+            oCommand.CommandType = CommandType.StoredProcedure;
+            object oReturnValue;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    if (parameters != null)
+                        oCommand.Parameters.AddRange(parameters);
+
+                    oCommand.Transaction = oTransaction;
+                    oReturnValue = oCommand.ExecuteScalar();
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oCommand.Dispose();
+                }
+            }
+            return oReturnValue;
+        }
+
+        #endregion//LEE LA PRIMERA COLUMNA Y FILA DE CADA TABLA
+
+        #region "EXECUTE NON QUERY"
+
+        public static int ExecuteNonQuery(string query)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(query, oConnection);
+
+            oCommand.CommandType = CommandType.Text;
+            int iReturnValue;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    oCommand.Transaction = oTransaction;
+                    iReturnValue = oCommand.ExecuteNonQuery();
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oCommand.Dispose();
+                }
+            }
+            return iReturnValue;
+        }
+
+        public static int ExecuteProcedureNonQuery(string procedureName)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(procedureName, oConnection);
+
+            oCommand.CommandType = CommandType.StoredProcedure;
+            int iReturnValue;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    oCommand.Transaction = oTransaction;
+                    iReturnValue = oCommand.ExecuteNonQuery();
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oCommand.Dispose();
+                }
+            }
+            return iReturnValue;
+        }
+
+        public static int ExecuteNonQuery(string procedureName, Dictionary<string, object> dictionary)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+
+            foreach (var par in dictionary)
+            {
+                parameters.Add(new SqlParameter(par.Key, par.Value));
+            }
+
+            return ExecuteNonQuery(procedureName, parameters.ToArray());
+        }
+
+        public static int ExecuteNonQuery(string procedureName, SqlParameter[] parameters)
+        {
+            SqlConnection oConnection = new SqlConnection(_connectionString);
+            SqlCommand oCommand = new SqlCommand(procedureName, oConnection);
+
+            oCommand.CommandType = CommandType.StoredProcedure;
+            int iReturnValue;
+            oConnection.Open();
+            using (SqlTransaction oTransaction = oConnection.BeginTransaction())
+            {
+                try
+                {
+                    if (parameters != null)
+                        oCommand.Parameters.AddRange(parameters);
+
+                    oCommand.Transaction = oTransaction;
+                    iReturnValue = oCommand.ExecuteNonQuery();
+                    oTransaction.Commit();
+                }
+                catch
+                {
+                    oTransaction.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    if (oConnection.State == ConnectionState.Open)
+                        oConnection.Close();
+                    oConnection.Dispose();
+                    oCommand.Dispose();
+                }
+            }
+            return iReturnValue;
+        }
+
+        #endregion
+
+    }
+}
--- CN/Producto.cs
+++ CN/Producto.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CD;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,173 @@ namespace CN
 {
     public class Producto // se agrego public para permitir accesso al Windows form  HomeDepot despues de agregar la referncia
     {
+        #region Propiedades
+        public int idProducto { get; }
+        public string unidadMed { get; }
+        public string descripcion { get; }
+        public float precioUnit { get; }
+        public bool esActivo { get; }
+        public DateTime fechaCreacion { get; }
+        #endregion
+
+        #region Contructores
+        public Producto(int _idProducto, string _unidadMed, string _descripcion,
+                        float _precioUnit, bool _esActivo)
+        {
+            idProducto = _idProducto;
+            unidadMed = _unidadMed;
+            descripcion = _descripcion;
+            precioUnit = _precioUnit;
+            esActivo = _esActivo;
+        }
+        public Producto(DataRow fila)
+        {
+            idProducto = fila.Field<int>("idProducto");
+            unidadMed = fila.Field<string>("unidadMed");
+            descripcion = fila.Field<string>("descripcion");
+            precioUnit = fila.Field<float>("precioUnit");
+            esActivo = fila.Field<bool>("esActivo");
+            fechaCreacion = fila.Field<DateTime>("fechaCreacion");
+        }
+
+
+        #endregion
+
+        #region Metodos y Funciones
+        public void guardar()
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+
+            
+            parametros.Add(new SqlParameter("@unidadMed", idProducto));
+            parametros.Add(new SqlParameter("@descripcion", descripcion));
+            parametros.Add(new SqlParameter("@precioUnit", precioUnit));
+            parametros.Add(new SqlParameter("@esActivo", idProducto));
+
+            
+
+            try
+            {
+                if (idProducto > 0)
+                {
+                    //Update
+                    parametros.Add(new SqlParameter("@idProducto", idProducto));
+                    if (DataBaseHelper.ExecuteNonQuery("dbo.SPUProducto", parametros.ToArray()) == 0)
+                    {
+                        throw new Exception("No se actualizo el registro");
+                    }   
+                }
+                else
+                {
+                    //Insert
+                    if (DataBaseHelper.ExecuteNonQuery("dbo.SPIProducto", parametros.ToArray()) == 0)
+                    {
+                        throw new Exception("No se creo el registro");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                throw new Exception(ex.Message);
+#else
+                throw new Exception("Ha ocurrido un error con la base de datos");
+#endif
+            }
+        }
+        public static void desactivar(int idProducto,bool esActivo = false)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+
+            parametros.Add(new SqlParameter("@idProducto", idProducto));
+            parametros.Add(new SqlParameter("esActivo", esActivo));
+
+            try
+            {
+                if (DataBaseHelper.ExecuteNonQuery("dbo.SPDProducto", parametros.ToArray()) == 0)
+                {
+                    throw new Exception("No se desactivo el registro");
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                throw new Exception(ex.Message);
+#else
+                throw new Exception("Ha ocurrido un error con la base de datos");
+#endif
+            }            
+        }
+        public static Producto buscarPorID(int idProducto)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idProducto", idProducto));
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                DataBaseHelper.Fill(dt, "dbo.SPSProductos", parametros.ToArray());
+
+                Producto resultado = null;
+
+                foreach (DataRow fila in dt.Rows)
+                {
+                    resultado = new Producto(fila);
+                    break;
+                }
+
+                if (resultado == null)
+                {
+                    throw new Exception("No se han encontrado coincidencias.");
+                }
+
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                throw new Exception(ex.Message);
+#else
+                throw new Exception("Ha ocurrido un error con la base de datos");
+#endif
+            }
+        }
+        public static List<Producto> traerTodos(bool filtrarSoloActivos = false)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+
+            if (filtrarSoloActivos)
+            {
+                parametros.Add(new SqlParameter("@esActivo", true));
+            }
+            
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                DataBaseHelper.Fill(dt, "dbo.SPSProductos", parametros.ToArray());
+
+                List<Producto> listado = new List<Producto>();
+
+                foreach (DataRow fila in dt.Rows)
+                {
+                    listado.Add(new Producto(fila));
+
+                }
+
+                return listado;
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                throw new Exception(ex.Message);
+#else
+                throw new Exception("Ha ocurrido un error con la base de datos");
+#endif
+            }
+        }
+#endregion
     }
 }
